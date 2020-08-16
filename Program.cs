607ae532a1c7772112a2dd629ceb769017bb04f5
BijanProgrammer <bijaneisapour@gmail.com>
2020-08-16 13:39:34 +0430

    Just a simple app

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CICD {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("This is just a simple app :D");
+        }
+    }
+}
